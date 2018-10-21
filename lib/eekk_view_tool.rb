9d065ae7d41122bd4d58e6bf715850ef98764761
deveye <malvy.duo@gmail.com>
2018-10-21 19:57:40 +0300

    eekk init commit

---
+++ lib/eekk_view_tool.rb
@@ -0,0 +1,6 @@
+require "eekk_view_tool/version"
+require "eekk_view_tool/renderer"
+
+module EekkViewTool
+  # Your code goes here...
+end
---
+++ lib/eekk_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EekkViewTool
+  VERSION = "0.1.0"
+end
